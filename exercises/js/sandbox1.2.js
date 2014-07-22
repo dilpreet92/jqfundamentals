792eeb13763c71f308d62438210ccdb608907745
Dilpreet Singh <dilpreet@vinsol.com>
2014-07-22 12:32:43 +0530

    Appending individually the headings and paragargraph and modifyng the last div tag by creating a clone of the image and then appending it to the currnent div

--- exercises/js/sandbox1.2.js
+++ exercises/js/sandbox1.2.js
@@ -8,11 +8,16 @@ $(document).ready(function() {
   $("#myList li:odd").remove();
 
   //Add another h2 and another paragraph to the last div.module
-  $("<h2>Hello</h2> <p>World!!!</p>").appendTo("div.module:last");
+  var heading = $("<h2>This is a new Heading</h2>");
+  var paragraph = $("<p>This is a new paragraph</p>");
+  heading.appendTo("div.module:last");
+  paragraph.appendTo("div.module:last");
 
   //Add another option to the select element; give the option the value "Wednesday"
-  $("select").append("<option value='wednesday'>Wednesday</option>");
+  var newOption = $("<option>Wednesday</option>").attr("value", "Wednesday");
+  $("select").append(newOption);
 
   //Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it
-  $("<div>Hello</div>").attr("class", "module").insertAfter("div.module:last").append("<img src='images/bread.jpg' alt='bread' />");
+  var lastDiv = $("<div></div>").attr("class", "module").insertAfter("div.module:last");
+  $("img[alt='bread']").clone().appendTo(lastDiv);
 });
